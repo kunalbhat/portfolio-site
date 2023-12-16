9a709d7101e2bed9784aae727878b4c74be792cd
Kunal Bhat <kunalbhat@gmail.com>
2023-12-16 13:22:23 -0600

    play around with content more

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -58,14 +58,14 @@ export function Home() {
       <section className="content-container md:flex md:gap-12 mt-0 md:mb-6 lg:mb-12">
         <animated.div className="w-full" style={styles}>
           <h1 className="lg:mx-12">
-            Hi, I'm Kunal &ndash; a UX design lead in Chicago.
+            Hi, I'm Kunal &ndash; a product design lead in Chicago.
           </h1>
-          <p className="lg:mx-12 lg:max-w-4xl">
+          <h3 className="lg:mx-12 lg:max-w-4xl leading-relaxed font-normal">
             I combine 16 years of building and delivering user experiences in
             roles as a designer, product manager, and software engineer. I'm a
             systems thinker, technically oriented, and highly comfortable in new
-            and ambiguous problem areas.
-          </p>
+            and ambiguous problem spaces.
+          </h3>
         </animated.div>
       </section>
       <div className="section-divider -mt-6" />
@@ -74,22 +74,25 @@ export function Home() {
           <h2>Experience</h2>
         </header>
         <div className="lg:grid grid-cols-3 gap-8">
-          <div className="case-study transition-all col-span-1">
+          <div className="case-study galactic transition-all col-span-1">
             <h3>Galactic</h3>
-            <h6>(RIP crypto, closed up shop)</h6>
+            <h6>Product Design Engineer</h6>
             <p>I built financial products for web3 users.</p>
+            <h6>(RIP crypto, closed up shop)</h6>
           </div>
-          <div className="case-study col-span-1" onClick={expandSection}>
+          <div className="case-study paypal col-span-1" onClick={expandSection}>
             <h3>PayPal / Braintree</h3>
+            <h6>Senior Technical Product Manager</h6>
             <p>
               I managed IAM products, launched a new version of the Braintree
               merchant dashboard, and established a merchant data platform.
             </p>
           </div>
-          <div className="case-study col-span-1" onClick={expandSection}>
+          <div className="case-study modest col-span-1" onClick={expandSection}>
             <h3>Modest</h3>
-            <h6>(acquired by PayPal)</h6>
+            <h6>Product Design Engineer</h6>
             <p>I designed and built a contextual commerce platform.</p>
+            <h6>(acquired by PayPal)</h6>
           </div>
           <div
             className="case-study dscout lg:col-span-1"
@@ -122,16 +125,13 @@ export function Home() {
       <section className="content-container">
         <header className="section-header">
           <h2>Projects</h2>
-          {/* <p>A few larger case studies to showcase that I'm most proud of.</p> */}
         </header>
         <div className="lg:grid grid-flow-row gap-16 grid-cols-5">
           <aside
-            className={`skyteller-cover static mx-auto w-full mb-8 lg:mb-0 flex col-span-3 shadow-xl rounded-2xl bg-indigo-950 items-center justify-center h-96 ${
-              expanded
-                ? "fixed top-20 left-0 right-0 bottom-0 w-2/3 mx-auto transition-all"
-                : ""
+            className={`skyteller-cover mx-auto w-full mb-8 lg:mb-0 flex col-span-3 shadow-xl rounded-2xl bg-indigo-950 items-center justify-center h-96 ${
+              expanded ? "focus" : ""
             }`}
-            // onClick={expandSection}
+            onClick={expandSection}
           >
             <SkytellerLogo />
           </aside>
@@ -165,9 +165,7 @@ export function Home() {
               Read on
             </a>
           </article>
-          <aside className="braintree-cover mb-8 lg:mb-0 flex col-span-3 shadow-xl rounded-2xl bg-white items-center justify-center h-80 md:h-96">
-            <p>Test</p>
-          </aside>
+          <aside className="braintree-cover mb-8 lg:mb-0 flex col-span-3 shadow-xl rounded-2xl bg-white items-center justify-center h-80 md:h-96"></aside>
         </div>
       </section>
       <div className="section-divider"></div>
